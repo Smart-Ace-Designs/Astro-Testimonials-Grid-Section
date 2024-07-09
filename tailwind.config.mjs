e1a1d5d14819750e2700346833eeccb890abd652
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-07-08 23:38:04 -0500

    Initial Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryModerateViolet: "hsl(263, 55%, 52%)",
+        primaryVeryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+        primaryVeryDarkBlackishBlue: "hsl(219, 29%, 14%)",
+        primaryWhite: "hsl(0, 0%, 100%)",
+        secondaryLightGray: "hsl(0, 0%, 81%)",
+        secondaryGrayishBlue: "hsl(210, 46%, 95%)",
+      },
+      fontFamily: {
+        barlow: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
